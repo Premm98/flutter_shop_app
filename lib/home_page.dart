622e86960bb6991a4cd80a6ebd5864e99ed38b83
Premm98 <prakashprem.pp2@gmail.com>
2024-02-25 10:55:40 +0530

    Added products page with Scrollable view, colors to the container of each product.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shop_app_flutter/global_variables.dart';
+import 'package:shop_app_flutter/product_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -91,6 +93,22 @@ class _HomePageState extends State<HomePage> {
                       ));
                 }),
           ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+                return ProductCard(
+                  title: product['title'] as String,
+                  price: product['price'] as double,
+                  image: product['imageUrl'] as String,
+                  backgroundColor: index.isEven
+                      ? const Color.fromRGBO(216, 240, 253, 1)
+                      : const Color.fromRGBO(245, 247, 249, 1),
+                );
+              },
+            ),
+          )
         ],
       ),
     ));
--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,22 @@ class MyApp extends StatelessWidget {
           seedColor: Color.fromRGBO(254, 206, 1, 1),
           primary: Color.fromRGBO(254, 206, 1, 1),
         ),
-        inputDecorationTheme: InputDecorationTheme(
+        inputDecorationTheme: const InputDecorationTheme(
             hintStyle: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 16,
             ),
             prefixIconColor: Color.fromRGBO(119, 119, 119, 1)),
+        textTheme: const TextTheme(
+          titleMedium: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+          bodySmall: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
       ),
       home: HomePage(),
     );
